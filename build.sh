288f10b0866b19be402676cf83a639e6499ed5db
Ritchie Mwewa <74001397+rly0nheart@users.noreply.github.com>
2026-01-25 05:47:08 +0200

    update: make build script 'smarter' by downloading the latest hugo version instead of hardcoding the version

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-curl -sL https://github.com/gohugoio/hugo/releases/download/v0.152.2/hugo_extended_0.152.2_linux-amd64.tar.gz | tar xz
+LATEST=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep '"tag_name"' | cut -d'"' -f4)
+VERSION=${LATEST#v}
+echo "Installing Hugo $VERSION"
+curl -sL "https://github.com/gohugoio/hugo/releases/download/${LATEST}/hugo_extended_${VERSION}_linux-amd64.tar.gz" | tar xz
 ./hugo --gc --minify
