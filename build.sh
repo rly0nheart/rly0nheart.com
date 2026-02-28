f1f86647a667d14d233bf6095aa6b083721c6a85
Ritchie Mwewa <74001397+rly0nheart@users.noreply.github.com>
2026-02-28 02:22:24 +0200

    potential fix for failed deploy

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-LATEST=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | grep '"tag_name"' | cut -d'"' -f4)
-VERSION=${LATEST#v}
-echo "Installing Hugo $VERSION"
-curl -sL "https://github.com/gohugoio/hugo/releases/download/${LATEST}/hugo_extended_${VERSION}_linux-amd64.tar.gz" | tar xz
+VERSION="0.157.0"
+echo "Installing Hugo ${VERSION}"
+curl -sL "https://github.com/gohugoio/hugo/releases/download/v${VERSION}/hugo_extended_${VERSION}_linux-amd64.tar.gz" | tar xz
 ./hugo --gc --minify
